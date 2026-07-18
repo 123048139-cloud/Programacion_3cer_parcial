ea19da1574d33c42ae4e25b5d9f7cb1ba13ab3c2
Arturo <123048139@upq.edu.mx>
2026-07-17 22:54:30 -0600

    Práctica No.20 Conexión Móvil API: Select & Insert

--- usuarioApi/screens/ConsultaUsuariosScreen.js
+++ usuarioApi/screens/ConsultaUsuariosScreen.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import {SafeAreaView,View,Text,FlatList,StyleSheet,
-} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, FlatList, StyleSheet } from "react-native";
 
 export default function ConsultaUsuariosScreen() {
+  const [usuarios, setUsuario] = useState([]);
 
-  const usuarios = [
-    { id: '1', nombre: 'Isay Guerra', edad: 22 },
-    { id: '2', nombre: 'Ana López', edad: 19 },
-    { id: '3', nombre: 'Carlos Gonzalez', edad: 25 },
-    { id: '4', nombre: 'Bjork Guerra', edad: 21 },
-    { id: '5', nombre: 'Luisa Martínez', edad: 28 },
-  ];
+  const obtenerUsuarios = async () => {
+    try {
+      const respuesta = await fetch("http://localhost:5000/v1/usuarios");
+      const datos = await respuesta.json();
+      console.log("Respuesta API: ", datos);
+      setUsuario(datos.usuarios);
+    } catch (error) {
+      console.log("Error API: ", error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerUsuarios();
+  }, []);
 
   const renderTarjeta = ({ item }) => (
     <View style={styles.card}>
-
       <Text style={styles.nombre}>{item.nombre}</Text>
 
       <View style={styles.linea}></View>
 
-      <Text style={styles.info}>
-        Edad: {item.edad} años
-      </Text>
-
+      <Text style={styles.info}>Edad: {item.edad} años</Text>
     </View>
   );
 
   return (
-
     <SafeAreaView style={styles.container}>
-
-      <Text style={styles.titulo}>
-        Lista de Usuarios
-      </Text>
+      <Text style={styles.titulo}>Lista de Usuarios</Text>
 
       <FlatList
         data={usuarios}
@@ -41,36 +40,33 @@ export default function ConsultaUsuariosScreen() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 20 }}
       />
-
     </SafeAreaView>
   );
-  
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    backgroundColor: '#F5F7FA',
+    backgroundColor: "#F5F7FA",
     padding: 20,
   },
 
   titulo: {
     fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#1F2937',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#1F2937",
     marginBottom: 20,
   },
 
   card: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     borderRadius: 15,
     padding: 18,
     marginBottom: 15,
     elevation: 4,
 
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.15,
     shadowRadius: 5,
     shadowOffset: {
@@ -81,19 +77,18 @@ const styles = StyleSheet.create({
 
   nombre: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#2563EB',
+    fontWeight: "bold",
+    color: "#2563EB",
   },
 
   linea: {
     height: 1,
-    backgroundColor: '#E5E7EB',
+    backgroundColor: "#E5E7EB",
     marginVertical: 10,
   },
 
   info: {
     fontSize: 16,
-    color: '#4B5563',
+    color: "#4B5563",
   },
-
 });
